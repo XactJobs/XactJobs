c143db044d817d5e17d5f646541c1e9746ba536a
enadzan <2304785+enadzan@users.noreply.github.com>
2025-06-21 10:55:11 +0200

    Move XactJobBase and XactJobHistory into separate files

---
+++ src/XactJobs/XactJobBase.cs
@@ -0,0 +1,56 @@
+﻿namespace XactJobs
+{
+    public class XactJobBase
+    {
+        public long Id { get; private set; }
+
+        /// <summary>
+        /// When should the job be executed
+        /// </summary>
+        public DateTime ScheduledAt { get; private set; }
+
+        /// <summary>
+        /// Assembly qualified name of the declaring type
+        /// </summary>
+        public string TypeName { get; private set; }
+
+        /// <summary>
+        /// Method name to be called
+        /// </summary>
+        public string MethodName { get; private set; }
+
+        /// <summary>
+        /// Arguments to be passed to the method
+        /// </summary>
+        public string MethodArgs { get; private set; }
+
+        public string Queue { get; private set; }
+
+        public string? PeriodicJobId { get; private set; }
+        public string? CronExpression { get; private set; }
+
+        public int ErrorCount { get; private set; }
+
+        public XactJobBase(long id,
+                           DateTime scheduledAt,
+                           string typeName,
+                           string methodName,
+                           string methodArgs,
+                           string queue,
+                           string? periodicJobId = null,
+                           string? cronExpression = null,
+                           int errorCount = 0)
+        {
+            Id = id;
+            ScheduledAt = scheduledAt;
+            TypeName = typeName;
+            MethodName = methodName;
+            MethodArgs = methodArgs;
+            Queue = queue;
+            PeriodicJobId = periodicJobId;
+            CronExpression = cronExpression;
+            ErrorCount = errorCount;
+        }
+    }
+
+} 
---
+++ src/XactJobs/XactJobHistory.cs
@@ -0,0 +1,69 @@
+﻿namespace XactJobs
+{
+    public class XactJobHistory: XactJobBase
+    {
+        public DateTime ProcessedAt { get; private set; }
+        public XactJobStatus Status { get; private set; }
+
+        public string? ErrorMessage { get; protected set; }
+        public string? ErrorStackTrace { get; protected set; }
+
+        public XactJobHistory(long id,
+                              DateTime processedAt,
+                              XactJobStatus status,
+                              DateTime scheduledAt,
+                              string typeName,
+                              string methodName,
+                              string methodArgs,
+                              string queue,
+                              string? periodicJobId = null,
+                              int errorCount = 0,
+                              string? cronExpression = null,
+                              string? errorMessage = null,
+                              string? errorStackTrace = null)
+            : base(id,
+                   scheduledAt,
+                   typeName,
+                   methodName,
+                   methodArgs,
+                   queue,
+                   periodicJobId,
+                   cronExpression,
+                   errorCount)
+        {
+            ProcessedAt = processedAt;
+            Status = status;
+            ErrorMessage = errorMessage;
+            ErrorStackTrace = errorStackTrace;
+        }
+
+        public static XactJobHistory CreateFromJob(XactJob job,
+                                                   XactJobPeriodic? periodicJob,
+                                                   DateTime processedAt,
+                                                   XactJobStatus status,
+                                                   int errorCount,
+                                                   Exception? ex)
+        {
+            var innerMostEx = ex;
+            while (innerMostEx?.InnerException != null)
+            {
+                innerMostEx = innerMostEx.InnerException;
+            }
+
+            return new XactJobHistory(job.Id,
+                                      processedAt,
+                                      status,
+                                      job.ScheduledAt,
+                                      job.TypeName,
+                                      job.MethodName,
+                                      job.MethodArgs,
+                                      job.Queue,
+                                      job.PeriodicJobId,
+                                      errorCount,
+                                      periodicJob?.CronExpression,
+                                      innerMostEx?.Message,
+                                      ex?.StackTrace);
+        }
+    }
+
+} 
